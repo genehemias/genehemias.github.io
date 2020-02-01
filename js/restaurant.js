a9399a2b3ee437ecc4fcda9b8e3b7600652b3432
genehemias <genehemias@users.noreply.github.com>
2020-02-01 16:19:08 -0500

    Restaurant (#3)
    * adds restaurant files
    
    * adds restaurant MVP
    
    * adds nav to restaurant picker

---
+++ js/restaurant.js
@@ -0,0 +1,70 @@
+
+(function () {
+    'use strict';
+
+    $(document).foundation();
+
+    const restaurants = [
+        "Zaxby's",
+        "Hardee's",
+        "Waffle House",
+        "Cracker Barrel",
+        "Pollo Tropical",
+        "Subway",
+        "Larry's Giant Subs",
+        "Tijuana Flats",
+        "Panera Bread",
+        "Moe's",
+        "Bono's",
+        "Sonny's",
+        "Five Guys",
+        "Freddy's",
+        "Panda Express"
+    ];
+
+    function btnSpinClick() {
+        let lblCurrentOption = $('#current-restaurant');
+
+        let randomIndex = Math.floor(Math.random() * restaurants.length);
+
+        let currentOption = restaurants[randomIndex];
+
+        console.log(currentOption);
+        console.log(lblCurrentOption);
+        console.log(lblCurrentOption.html());
+        console.log(lblCurrentOption.innerText);
+        lblCurrentOption.html(currentOption);
+
+        // btnVetoEnabled(true);
+        // btnSpinEnabled(false);
+    };
+
+    function btnVetoEnabled(enable) {
+        let btnVeto = $("#btnVeto");
+        if (enable) {
+            btnVeto.prop('disabled', false);
+            btnVeto.removeClass('disabled');
+        } else {
+            btnVeto.prop('disabled', true);
+            btnVeto.addClass('disabled');
+        }
+    }
+
+    function btnSpinEnabled(enable) {
+        let btnSpin = $("#btnSpin");
+        if (enable) {
+            btnSpin.removeClass('disabled');
+        } else {
+            btnSpin.addClass('disabled');
+        }
+    }
+
+    $('#btnSpin').click(() => {
+        btnSpinClick();        
+    });
+
+    // $("#btnVeto").click(() => {
+    //     btnSpinEnabled(true);
+    //     btnVetoEnabled(false);
+    // })
+})();
